568a95ba4ee03e1c9476adfce3225a13d0c502de
rafa_ce <rafasantcont@yahoo.com.br>
2013-08-13 01:45:30 -0300

    Sintatico: lerArquivo()

--- src/main/java/analise/sintatica/Sintatico.java
+++ src/main/java/analise/sintatica/Sintatico.java
@@ -1,7 +1,25 @@
 package analise.sintatica;
 
+import java.io.BufferedReader;
+
+import analise.lexica.Lexico;
+
 public class Sintatico {
 	
+	private Lexico lexico;
+	private Integer linha;
 	
+	public Sintatico() {
+		linha = 0;
+	}
+	
+	public void lerArquivo(BufferedReader arquivo) throws Throwable {
+		
+		while (arquivo.ready()) {
+			lexico.getNextToken(arquivo.readLine());
+			linha++;
+		}
+		arquivo.close();
+	}
 
 }
