4aa768a988f193ad401a23d6ea7fbac8efe4e357
rafa_ce <rafasantcont@yahoo.com.br>
2013-08-14 00:16:51 -0300

    1. Léxico lendo um arquivo.
    2. Correção do bug -> #2: Léxico - Reconhecer último token da linha.
    
    3. Arquivos de testes no resources.

--- src/main/java/analise/lexica/Lexico.java
+++ src/main/java/analise/lexica/Lexico.java
@@ -1,5 +1,9 @@
 package analise.lexica;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import utils.Token;
 import analise.lexica.automato.Automato;
 import analise.lexica.automato.Estado;
@@ -8,25 +12,54 @@ public class Lexico {
 	
 	private Automato automato;
 	private Integer coluna;
+	private Integer linha;
 	private Integer posicaoToken;
 	private TipoToken tipoToken;
-
-	public Lexico() {
+	private BufferedReader codigoFonte;
+	private String linhaAtual;
+	
+	public Lexico(String entrada) {
 		this.automato = AutomatoTiger.implementa();
 		this.coluna = 0;
+		this.linha = 0;
 		this.tipoToken = new TipoToken();
+		inicializa(entrada);
+		
+		try {
+			linhaAtual = lerArquivo();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void inicializa(String entrada) {
+		try {
+			codigoFonte = new BufferedReader(new FileReader(entrada));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
 	}
 
-	public Token getNextToken(String entrada) throws Throwable {
+	private String lerArquivo() throws Throwable {	
+		if (codigoFonte.ready())
+			 return codigoFonte.readLine() + " ";
+			
+		codigoFonte.close();
+		
+		return "";
+	}
+
+	public Token getNextToken() throws Throwable {
 
 		String valorDoToken = "";
 		Estado estadoAtual = automato.getEstadoInicial();
-
+		
 		posicaoToken = coluna;
 		
-		while (coluna < entrada.length()) {
+		while (coluna < linhaAtual.length() + 1) {
 			
-			Character cada = entrada.charAt(coluna);
+			Character cada = linhaAtual.charAt(coluna);
 			
 			try {
 				estadoAtual = automato.getProximoEstado(estadoAtual, cada.toString());
@@ -41,12 +74,14 @@ public class Lexico {
 
 		}
 		
-		if (coluna.equals(entrada.length())) {
+		if (coluna.equals(linhaAtual.length())) {
 			coluna = 0;
+			linhaAtual = lerArquivo();
 		}
 		
 		return montaToken(valorDoToken, posicaoToken, estadoAtual);
 	}
+	
 	
 	private Token montaToken(String valor, Integer posicaoToken, Estado estado) {
 		
--- src/test/java/analise/lexica/LexicoTest.java
+++ src/test/java/analise/lexica/LexicoTest.java
@@ -11,39 +11,35 @@ public class LexicoTest {
 	@Test
 	public void identificaUmaLinha() throws Throwable {
 		
-		String entrada = "while i < 100 do i = j + i\n";
+		Lexico lexico = new Lexico("src/test/resources/arquivoLinhaUnica.txt");
 		
-		Lexico lexico = new Lexico();
-		
-		assertToken("while", 0, "palavraChave", lexico.getNextToken(entrada));
-		assertToken("i", 6, "identificador", lexico.getNextToken(entrada));
-		assertToken("<", 8, "simbolo", lexico.getNextToken(entrada));
-		assertToken("100", 10, "numero", lexico.getNextToken(entrada));
-		assertToken("do", 14, "palavraChave", lexico.getNextToken(entrada));
-		assertToken("i", 17, "identificador", lexico.getNextToken(entrada));
-		assertToken("=", 19, "simbolo", lexico.getNextToken(entrada));
-		assertToken("j", 21, "identificador", lexico.getNextToken(entrada));
-		assertToken("+", 23, "simbolo", lexico.getNextToken(entrada));
-		assertToken("i", 25, "identificador", lexico.getNextToken(entrada));
+		assertToken("while", 0, "palavraChave", lexico.getNextToken());
+		assertToken("i", 6, "identificador", lexico.getNextToken());
+		assertToken("<", 8, "simbolo", lexico.getNextToken());
+		assertToken("100", 10, "numero", lexico.getNextToken());
+		assertToken("do", 14, "palavraChave", lexico.getNextToken());
+		assertToken("i", 17, "identificador", lexico.getNextToken());
+		assertToken("=", 19, "simbolo", lexico.getNextToken());
+		assertToken("j", 21, "identificador", lexico.getNextToken());
+		assertToken("+", 23, "simbolo", lexico.getNextToken());
+		assertToken("i", 25, "identificador", lexico.getNextToken());
 	}
 	
 	@Test
 	public void identificaDuasLinhas() throws Throwable {
-		String entrada1 = "while i < 100 do\n";
-		String entrada2 = "i = j + i\n";
 		
-		Lexico lexico = new Lexico();
+		Lexico lexico = new Lexico("src/test/resources/arquivoDuasLinhas.txt");
 		
-		assertToken("while", 0, "palavraChave", lexico.getNextToken(entrada1));
-		assertToken("i", 6, "identificador", lexico.getNextToken(entrada1));
-		assertToken("<", 8, "simbolo", lexico.getNextToken(entrada1));
-		assertToken("100", 10, "numero", lexico.getNextToken(entrada1));
-		assertToken("do", 14, "palavraChave", lexico.getNextToken(entrada1));
-		assertToken("i", 0, "identificador", lexico.getNextToken(entrada2));
-		assertToken("=", 2, "simbolo", lexico.getNextToken(entrada2));
-		assertToken("j", 4, "identificador", lexico.getNextToken(entrada2));
-		assertToken("+", 6, "simbolo", lexico.getNextToken(entrada2));
-		assertToken("i", 8, "identificador", lexico.getNextToken(entrada2));
+		assertToken("while", 0, "palavraChave", lexico.getNextToken());
+		assertToken("i", 6, "identificador", lexico.getNextToken());
+		assertToken("<", 8, "simbolo", lexico.getNextToken());
+		assertToken("100", 10, "numero", lexico.getNextToken());
+		assertToken("do", 14, "palavraChave", lexico.getNextToken());
+		assertToken("i", 0, "identificador", lexico.getNextToken());
+		assertToken("=", 3, "simbolo", lexico.getNextToken());
+		assertToken("j", 5, "identificador", lexico.getNextToken());
+		assertToken("+", 7, "simbolo", lexico.getNextToken());
+		assertToken("i", 9, "identificador", lexico.getNextToken());
 	}
 	
 	private void assertToken(String valor, Integer posicao, String tipo, Token token) {
